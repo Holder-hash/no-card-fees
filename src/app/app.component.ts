1574731493e54193199e3624cbb7b88eb412c037
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-04-21 22:34:09 +0300

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {OverviewComponent} from "./overview/overview.component";
+import {AdvantagesComponent} from "./advantages/advantages.component";
+import {MobileComponent} from "./mobile/mobile.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, OverviewComponent, AdvantagesComponent, MobileComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
